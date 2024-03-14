7623525b71c8b5eea235c2d68f467c4df7d02024
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-03-14 16:03:57 +0300

    add

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+
 function addingEventListener() {
+    function clickAlert() {
+        alert('I was clicked!');
+      }
+      
+      input.addEventListener('click', clickAlert); 
 }
+const input = document.getElementById('button');
+
+
